77c0a1735dc89f15c29a7242ca2574720952ab4a
brokeashell62 <brokeashell62@gmail.com>
2025-09-13 16:49:58 +0530

    Update GameRendererMixin.java

--- src/main/java/walksy/crosshairaddons/mixin/GameRendererMixin.java
+++ src/main/java/walksy/crosshairaddons/mixin/GameRendererMixin.java
@@ -1,39 +1,21 @@
 package walksy.crosshairaddons.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.render.BufferBuilderStorage;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.RenderTickCounter;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.render.VertexConsumerProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import walksy.crosshairaddons.manager.ConfigManager;
-import walksy.crosshairaddons.manager.CrosshairRendererManager;
+import org.spongepowered.asm.mixin.Inject;
+import org.spongepowered.asm.mixin.At;
 
 @Mixin(GameRenderer.class)
-public abstract class GameRendererMixin {
+public class GameRendererMixin {
 
-    @Shadow
-    @Final
-    MinecraftClient client;
-
-    @Shadow
-    @Final
-    private BufferBuilderStorage buffers;
-
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V"))
-    public void onRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci)
-    {
-        if (!ConfigManager.modEnabled) return;
-        DrawContext drawContext = new DrawContext(this.client, this.buffers.getEntityVertexConsumers());
-        CrosshairRendererManager.INSTANCE.renderCrosshair(drawContext);
+    @Inject(method = "render", at = @At("HEAD"))
+    private void beforeRender(MatrixStack matrices, float tickDelta, long startTime, boolean tick, float frameTime, CallbackInfo ci) {
+        DrawContext drawContext = new DrawContext(MinecraftClient.getInstance(), VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer()));
+        // Adjusted method signature and handling
     }
 }
