c494447186bab949de5c6958efadf81c11488256
brokeashell62 <brokeashell62@gmail.com>
2025-09-14 11:56:31 +0530

    Update DrawContextMixin.java

--- src/main/java/walksy/crosshairaddons/mixin/DrawContextMixin.java
+++ src/main/java/walksy/crosshairaddons/mixin/DrawContextMixin.java
@@ -4,19 +4,14 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Injection;
-import org.spongepowered.asm.mixin.At;
 
 @Mixin(DrawContext.class)
-public class DrawContextMixin {
+public abstract class DrawContextMixin {
+
+    // These methods exist in Yarn mappings for DrawContext
+    @Shadow
+    public abstract void drawTexture(Identifier texture, int x, int y, int u, int v, int width, int height, int textureWidth, int textureHeight);
 
     @Shadow
-    private void drawGuiTexture(Identifier identifier, int x, int y, int width, int height) {
-        // Implement the shadow method for rendering textures here
-    }
-
-    @Inject(method = "drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", at = @At("HEAD"), cancellable = true)
-    private void injectDrawGuiTexture(Identifier identifier, int x, int y, int width, int height, CallbackInfo ci) {
-        drawGuiTexture(identifier, x, y, width, height);
-    }
+    public abstract void drawTexture(Identifier texture, int x, int y, int u, int v, int width, int height);
 }
